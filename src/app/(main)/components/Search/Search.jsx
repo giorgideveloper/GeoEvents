dc3e796a15217afd5ff1dbd6cff479d8951cdb96
giorgideveloper <gorgiladze888@gmail.com>
2023-09-12 17:57:52 +0400

    add Search

--- src/app/(main)/components/Search/Search.jsx
+++ src/app/(main)/components/Search/Search.jsx
@@ -33,7 +33,9 @@ export default function Search() {
 				className='block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 				placeholder='Search...'
 			/>
-			<SearchContainer item={item} />
+			<div className='absolute dark:bg-gray-700/50 w-full text-gray-50 rounded-b-lg'>
+				<SearchContainer item={item} />
+			</div>
 		</>
 	);
 }
--- src/app/(main)/components/Search/SearchContainer/SearchContainer.jsx
+++ src/app/(main)/components/Search/SearchContainer/SearchContainer.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React, { useEffect } from 'react';
 
 export default function SearchContainer({ item }) {
@@ -5,5 +7,30 @@ export default function SearchContainer({ item }) {
 		console.log(item);
 	}, [item]);
 
-	return <></>;
+	return (
+		<>
+			{item
+				? item.map(item => (
+						<div className=' pb-5 ' key={item.id}>
+							<Link href={`events/${item.id}`} alt=''>
+								<div className=' h-full bg-white border border-gray-200  shadow search-card '>
+									<Image
+										width={350}
+										height={197}
+										src={item.image}
+										alt={'image'}
+									/>
+
+									<div className='p-5 h-full'>
+										<h5 className='mb-2 text-xs font-bold tracking-tight text-gray-900'>
+											{item.name_ka}
+										</h5>
+									</div>
+								</div>
+							</Link>
+						</div>
+				  ))
+				: ''}
+		</>
+	);
 }

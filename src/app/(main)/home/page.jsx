ed703945b7af61b4dfe1c4d0ea4d5a5141a07bfa
giorgideveloper <gorgiladze888@gmail.com>
2023-09-18 18:24:39 +0400

    add metadata

--- src/app/(main)/home/page.jsx
+++ src/app/(main)/home/page.jsx
@@ -2,17 +2,12 @@ import Navbar from '../components/Navbar';
 import Events from '../components/EventsPost';
 import Slider from '../components/slider/slider';
 import Footer from '../components/footer/';
-import { Metadata } from 'next';
-import Head from 'next/head';
+
 // either Static metadata
 
 export default function Page() {
 	return (
 		<>
-			<Head>
-				<title>Your Page Title</title>
-				<meta name='description' content='This is the page description.' />
-			</Head>
 			<div className='page-container'>
 				<Navbar />
 				<Slider />
